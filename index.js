a2f2d9819fae1fb0b421b8150513937ee565e5c3
IObregon-plain <iobregon@plainconcepts.com>
2021-10-18 12:28:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const github = require('@actions/github');
 try {
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}`);
+    console.log(`Hello ${nameToGreet.split('').join(' ')}`);
     const time = (new Date()).toTimeString();
     core.setOutput('time', time);
 }catch (error){
